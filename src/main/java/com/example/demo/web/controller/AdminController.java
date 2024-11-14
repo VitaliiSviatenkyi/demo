816f66b5c0548a83709fa10e0518a093f2c56634
Vitalii Sviatenkyi <vexa2009@gmail.com>
2024-11-14 09:36:33 +0100

    #3

--- src/main/java/com/example/demo/web/controller/AdminController.java
+++ src/main/java/com/example/demo/web/controller/AdminController.java
@@ -1,9 +1,6 @@
 package com.example.demo.web.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,15 +13,12 @@ public class AdminController {
     @GetMapping("/admin")
     @PreAuthorize("hasRole('ADMIN') or principal.username == 'admin'")
     public String getAdminPage() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return "Welcome ADMIN";
+    }
 
-        String username = authentication.getName();
-
-        String roles = authentication.getAuthorities().stream()
-                .map(GrantedAuthority::getAuthority)
-                .reduce((r1, r2) -> r1 + ", " + r2)
-                .orElse("No roles");
-
-        return "Welcome " + username + "! Your roles are: " + roles;
+    @GetMapping("/employee")
+    @PreAuthorize("hasRole('EMPLOYEE')")
+    public String getEmployeePage() {
+        return "Welcome EMPLOYEE";
     }
 }
